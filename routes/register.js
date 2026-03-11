0ff13f9ae48b10fbffb771f1d1c51284a1ba1709
mvbrahm <55369617+mvbrahm@users.noreply.github.com>
2026-03-11 22:21:33 +0000

    feat: update registration form with additional fields and improved layout

--- routes/register.js
+++ routes/register.js
@@ -2,12 +2,12 @@ var express = require('express');
 var router = express.Router();
 
 /* GET register page. */
-router.get('/', function(req, res, next) {
+router.get('/', function (req, res, next) {
   res.render('register', { title: 'Register' });
 });
 
 // Handle inputs from the register form and display in the console.log
-router.post('/', function(req, res, next) {
+router.post('/', function (req, res, next) {
   console.log(req.body)
   console.log('username is ' + req.body.username)
 });
