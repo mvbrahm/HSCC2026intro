38e8728951f4ef08bc4bd8f3f5fe3b3e35e90e78
mvbrahm <55369617+mvbrahm@users.noreply.github.com>
2026-03-11 15:26:36 +0000

    feat: enhance registration form with state selection and bio textarea

--- routes/register.js
+++ routes/register.js
@@ -9,8 +9,13 @@ router.get('/', function(req, res, next) {
 // Handle inputs from the register form and display in the console.log
 router.post('/', function(req, res, next) {
   console.log(req.body)
+  console.log('username is ' + req.body.username)
 });
 
+// router.post('/', (req, res) => {
 
+//   res.send('Hello ' + req.body.username + ', you have successfully registered with the email: ' + req.body.email);
+
+// })
 
 module.exports = router;
