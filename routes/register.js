fd2432c03694d6b4017ff41f86f384940f3484c1
mvbrahm <55369617+mvbrahm@users.noreply.github.com>
2026-03-11 05:00:39 +0000

    feat: implement registration page and update navigation links

--- routes/register.js
+++ routes/register.js
@@ -1,9 +1,16 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
+/* GET register page. */
 router.get('/', function(req, res, next) {
-  res.render('register', { title: 'Express' });
+  res.render('register', { title: 'Register' });
 });
+
+// Handle inputs from the register form and display in the console.log
+router.post('/', function(req, res, next) {
+  console.log(req.body)
+});
+
+
 
 module.exports = router;
