f706d3e49bb332f6fb8482b9e84a956ad7e6e878
mvbrahm <55369617+mvbrahm@users.noreply.github.com>
2026-03-14 17:58:56 +0000

    class work

---
+++ routes/register.js
@@ -0,0 +1,16 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET register page. */
+router.get('/', function (req, res, next) {
+  res.render('register', { title: 'Register' });
+});
+
+router.post('/', function (req, res, next) {
+  console.log(req.body);
+  res.render('register', { title: 'Register' });
+});
+
+
+
+module.exports = router;
