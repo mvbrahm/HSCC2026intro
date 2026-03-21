d0e33bc52e1d3b668b487408748ae11dd0e85ad2
kempensp <kempensp@milwaukee.k12.wi.us>
2026-03-21 16:27:00 +0000

    error fix

--- public/javascripts/registerscript.js
+++ public/javascripts/registerscript.js
@@ -1,1 +1,9 @@
-alert('Register page open');
+const pwinput=document.querySelector("#password");
+const pwstrength=document.getElementById("pwstrength");
+
+pwinput.addEventListener("input", updatePasswordStrength);
+
+function updatePasswordStrength(e){
+    pwstrength.textContent=e.target.value;
+}
+
