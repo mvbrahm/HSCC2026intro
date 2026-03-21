a01ffbcc0d9ead7e5c15ef7216a8806f5e736b29
kempensp <kempensp@milwaukee.k12.wi.us>
2026-03-21 16:48:06 +0000

    color works

--- public/javascripts/registerscript.js
+++ public/javascripts/registerscript.js
@@ -4,6 +4,18 @@ const pwstrength=document.getElementById("pwstrength");
 pwinput.addEventListener("input", updatePasswordStrength);
 
 function updatePasswordStrength(e){
-    pwstrength.textContent=e.target.value;
+    var pwlength=e.target.value.length; 
+    if (pwlength<=10){
+        pwstrength.textContent="Weak";
+        pwstrength.style.color="red";
+    }
+    else if (pwlength>17){
+        pwstrength.textContent="Strong";
+        pwstrength.style.color="green";
+    }
+    else {
+        pwstrength.textContent="Moderate";
+        pwstrength.style.color="yellow";
+    }
 }
 
