72e5068aec71dfa31081a312c5d59573fe838535
kempensp <kempensp@milwaukee.k12.wi.us>
2026-03-28 17:24:44 +0000

    license code

--- routes/register.js
+++ routes/register.js
@@ -3,12 +3,55 @@ var router = express.Router();
 
 /* GET register page. */
 router.get('/', function (req, res, next) {
-  res.render('register', { title: 'Register' });
+  res.render('register', { title: 'Register' , licensecode: "To be determined"});
 });
 
 router.post('/', function (req, res, next) {
-  console.log(req.body);
-  res.render('register', { title: 'Register' });
+  
+  //Test form submission
+  if (process.env.PRODUCTION=="false") {
+    //console.log(req.body); 
+    console.log(req.body.birthdate);
+    console.log(typeof req.body.birthdate);
+    console.log(req.body.gender);
+  }
+
+  
+  let dob=req.body.birthdate;
+  let gender=req.body.gender;
+
+  //Slice dob string into month, day, and year
+  let year=dob.slice(0,4);
+
+  let month=Number(dob.slice(5,7));
+  let day=Number(dob.slice(8)); 
+
+  if (process.env.PRODUCTION=="false") {
+    console.log(year);
+    console.log(month);
+    console.log(day);
+  }
+
+  //Calculate code here
+  let yy=year.slice(2);
+  let ddd=(month-1)*40+day;
+
+  if (process.env.PRODUCTION=="false") {
+    console.log(yy);
+    console.log(ddd);
+  }
+
+  if (gender=='female'){
+    ddd+=500;
+  }
+
+  ddd=ddd.toString();
+  //Pad to 3 characters
+  ddd=String(ddd).padStart(3,'0');
+  //Get final code
+  code=yy+ddd;
+  
+  res.render('register', { title: 'Register' , licensecode: code});
 });
 
 
