90a2c3b9cf95af5905d117fca278d6a44e87f4f5
kempensp <kempensp@milwaukee.k12.wi.us>
2026-04-25 14:50:03 +0000

    full toggle l v d mode

--- public/javascripts/togglescript.js
+++ public/javascripts/togglescript.js
@@ -1,8 +1,12 @@
 document.addEventListener("DOMContentLoaded", () => {
   const contrastButton = document.querySelector("#contrast-toggle");
   const container = document.querySelector(".container");
+  const containerfl = document.querySelector(".container-fluid");
+  const bd = document.querySelector("body");
 
   contrastButton.addEventListener("click", () => {
     container.classList.toggle("high-contrast");
+    containerfl.classList.toggle("high-contrast");
+    bd.classList.toggle("high-contrast");
   });
 });
